56abce90f83c36f98652c4f0f4300d58d4b7ffe4
Ehsan Barkhordar <barkhordar.me@gmail.com>
2022-11-29 16:34:28 +0330

    add formatter docstring

--- app/services.py
+++ app/services.py
@@ -3,11 +3,20 @@ import math
 from app.constants import magnitudes
 
 
-def formatter(n):
-    n = float(n)
-    n_thousands_order = int(math.floor(0 if n == 0 else math.log10(abs(n)) / 3))
+def formatter(input_number):
+    """
+       Returns a truncated, prettified string version of a float number that supports millions, billions and trillions
+
+               Parameters:
+                       input_number (float): A float number
+
+               Returns:
+                       output (str): Prettified string version
+    """
+    input_number = float(input_number)
+    n_thousands_order = int(math.floor(0 if input_number == 0 else math.log10(abs(input_number)) / 3))
     magnitude_idx = max(0, min(len(magnitudes) - 1, n_thousands_order))
-    n_magnitude = n / 10 ** (3 * magnitude_idx)
+    n_magnitude = input_number / 10 ** (3 * magnitude_idx)
     if "{:.1f}".format(n_magnitude) == "{:.0f}".format(n_magnitude) + ".0":
         return '{:.0f}{}'.format(n_magnitude, magnitudes[magnitude_idx])
     else:
